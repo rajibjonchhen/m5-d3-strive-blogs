6a46746620343cd7af1f6fcde09d218d76117ac4
rajibjonchhen <rajib.jonchhen@gmail.com>
2022-01-26 14:00:03 +0000

    server is running

---
+++ src/server.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import listEndpoints from 'express-list-endpoints'
+import blogsRouter from './service/index.js'
+import cors from 'cors'
+const server = express()
+const port = 3001
+
+server.use(express.json())
+server.use(cors())
+
+console.table(listEndpoints(server))
+
+server.use("/blogs",blogsRouter)
+
+server.listen(port,() =>{
+console.log(`server is running in ${port}`)
+})
