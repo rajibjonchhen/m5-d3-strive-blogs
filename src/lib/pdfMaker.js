4bb9f9d0823793373179fe64e73b693714115e70
rajibjonchhen <rajib.jonchhen@gmail.com>
2022-02-02 19:20:36 +0000

    comments added

--- src/lib/pdfMaker.js
+++ src/lib/pdfMaker.js
@@ -25,89 +25,37 @@ export const generateBlogPDF = async (blog) => {
     const base64Image = `data:image/${extension};base64,${base64}`;
     imagePart = { image: base64Image, width: 500, margin: [0, 0, 0, 40] };
   }
+
+  let avatarPart = {};
+  if (blog.author.avatar) {
+    const response = await axios.get(blog.author.avatar, {
+      responseType: "arraybuffer",
+    });
+    const avatarURLParts = blog.author.avatar.split("/");
+    const fileName = avatarURLParts[blogCoverURLParts.length - 1];
+    const [id, extension] = fileName.split(".");
+    const base64 = response.data.toString("base64");
+    const base64Image = `data:image/${extension};base64,${base64}`;
+   avatarPart = { image: base64Image, width: 50, margin: [0, 0, 0, 40] };
+  }
+
   const docDefinition = {
     content: [
       imagePart,
       { text: blog.title, fontSize: 20, bold: true, margin: [0, 0, 0, 40] },
       { text: striptags(blog.content), lineHeight: 2 },
+     ul=[
+        avatarPart,
+        { text:`${blog.comments[0].commentedAt}`, lineHeight: 2 },
+        { text:`${blog.comments[0].comment} ${blog.comments[0].rate}`, lineHeight: 2 },
+        
+        { text:`${blog.comments[1].commentedAt}`, lineHeight: 2 },
+        { text:`${blog.comments[1].comment} ${blog.comments[0].rate}`, lineHeight: 2 },
+        
+     ]
     ],
   };
 
   const pdfDoc = printer.createPdfKitDocument(docDefinition);
   return pdfDoc;
 };
-// import PdfPrinter from "pdfmake"
-// import axios from "axios";
-
-
-
-
-// export const getPDFReadableStream = async (blog) => {
-//    const fonts = {
-//     Helvetica: {
-//         normal: "Helvetica",
-//         bold: "Helvetica-Bold",
-//         italics: "Helvetica-Italics",
-//       },
-//       };
-
-//   const printer = new PdfPrinter(fonts)
-
-//   let imagePart = {};
-
-//   if (blog.cover) {
-//     const response = await axios.get(blog.cover, {
-//       responseType: "arraybuffer",
-//     });
-//     const blogCoverURLParts = blog.cover.split("/");
-//     const fileName = blogCoverURLParts[blogCoverURLParts.length - 1];
-//     const [id, extension] = fileName.split(".");
-//     const base64 = response.data.toString("base64");
-//     const base64Image = `data:image/${extension};base64,${base64}`;
-//     imagePart = { image: base64Image, width: 500, margin: [0, 0, 0, 40] };
-//   }
-
-//   const docDefinition = {
-//     content: [
-//       {
-//         text: blog.title,
-//         style: "header",
-//       },
-//       imagePart,
-      
-//       {
-//           text: blog.content,
-//           style: ["quote", "small"],
-//         },
-//         {
-//             text: "Comments",
-//             style: "subheader",
-//           },
-//           `${blog.comment}`
-//     ],
-//     styles: {
-//       header: {
-//         fontSize: 18,
-//         bold: true,
-//       },
-//       subheader: {
-//         fontSize: 15,
-//         bold: true,
-//       },
-//       quote: {
-//         italics: true,
-//       },
-//       small: {
-//         fontSize: 8,
-//       },
-//     },
-//     defaultStyle: {
-//         font: "Helvetica",
-//       },
-//   }
-
-//   const pdfReadableStream = printer.createPdfKitDocument(docDefinition, {})
-//   pdfReadableStream.end()
-
-//   return pdfReadableStream
-// }
