ee0f3fbccd8af3489c1b0312135622f54ca5e8f7
rajibjonchhen <rajib.jonchhen@gmail.com>
2022-02-03 16:33:32 +0000

    env added api key added

--- src/lib/email-tools.js
+++ src/lib/email-tools.js
@@ -1,16 +1,16 @@
-   
+
 import sgMail from "@sendgrid/mail"
 
-sgMail.setApiKey("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")
+sgMail.setApiKey(process.env.SENDGRID_KEY)
 
-export const sendRegistrationEmail = async recipientAddress => {
-  const msg = {
+export const sendRegistrationEmail = async () => {
+const msg = {
     to: 'onlyrajib@gmail.com',
     from: process.env.SENDER_EMAIL, 
     subject: "Sending with SendGrid is Fun",
     text: "and easy to do anywhere, even with Node.js",
     html: "<strong>and easy to do anywhere, even with Node.js</strong>",
-  }
+}
 
-  await sgMail.send(msg)
+await sgMail.send(msg)
 }
